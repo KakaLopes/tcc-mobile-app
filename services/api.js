b26cb135fa71798de2e9370026aaf7ea2c89be43
Catalina Lopes <catalina-lopes@hotmail.com>
2026-05-21 21:24:07 +0100

    Update API URL to Cloudflare tunnel

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.0.9:3000",
+  baseURL: "https://neil-suitable-collect-simulation.trycloudflare.com",
 });
 
 export default api;
